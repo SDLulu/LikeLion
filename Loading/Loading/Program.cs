35a91de27b00236114f944d2070bf055dbc4a380
SDLulu <52689979+SDLulu@users.noreply.github.com>
2025-02-21 15:04:22 +0900

    Loading

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(1000); //1000이 1초 (1000밀리초)
+            Console.Clear(); //콘솔화면지우기
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(1000); //1000이 1초 (1000밀리초)
+            Console.Clear(); //콘솔화면지우기
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000); //1000이 1초 (1000밀리초)
+            Console.Clear(); //콘솔화면지우기
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(1000); //1000이 1초 (1000밀리초)
+            Console.Clear(); //콘솔화면지우기
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(1000); //1000이 1초 (1000밀리초)
+            Console.Clear(); //콘솔화면지우기
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(1000); //1000이 1초 (1000밀리초)
+            Console.Clear(); //콘솔화면지우기
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(1000); //1000이 1초 (1000밀리초)
+            Console.Clear(); //콘솔화면지우기
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(1000); //1000이 1초 (1000밀리초)
+            Console.Clear(); //콘솔화면지우기
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(1000); //1000이 1초 (1000밀리초)
+            Console.Clear(); //콘솔화면지우기
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(1000); //1000이 1초 (1000밀리초)
+            Console.Clear(); //콘솔화면지우기
+            Console.WriteLine("■■■■■■■■■■");
+            Thread.Sleep(1000); //1000이 1초 (1000밀리초)
+            Console.Clear(); //콘솔화면지우기
+            Console.WriteLine("텍스트 엘든링");
+
+            Console.WriteLine("스토리1");
+        }
+    }
+}
